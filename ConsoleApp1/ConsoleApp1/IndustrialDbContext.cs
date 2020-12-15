c191a2048e498c92e5181822ef05518f1f99b84b
然 张 <zhangranmm@126.com>
2020-12-15 15:06:10 +0800

    添加实体与dbcontext

---
+++ ConsoleApp1/ConsoleApp1/IndustrialDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class IndustrialDbContext: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Data> Datas { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Test> Tests { get; set; }
+
+
+    }
+}
