edfbf0ccdc0e470600fae05eb15e95806f59c278
然 张 <zhangranmm@126.com>
2020-12-15 17:38:33 +0800

    1

--- ConsoleApp1/ConsoleApp1/IndustrialDbContext.cs
+++ ConsoleApp1/ConsoleApp1/IndustrialDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Entity;
+using SQLite.CodeFirst;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,11 +11,18 @@ namespace Entities
 {
     public class IndustrialDbContext: DbContext
     {
+        public IndustrialDbContext():base("name=Sqlite")
+        { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<IndustrialDbContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Data> Datas { get; set; }
         public DbSet<Device> Devices { get; set; }
         public DbSet<Test> Tests { get; set; }
-
-
     }
 }
