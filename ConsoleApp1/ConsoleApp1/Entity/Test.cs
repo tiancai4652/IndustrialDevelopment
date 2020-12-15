b8590525615bd0d94eb5512b1f4945cd7f870531
然 张 <zhangranmm@126.com>
2020-12-15 11:33:49 +0800

    添加 用户，设备，测试，数据

---
+++ ConsoleApp1/ConsoleApp1/Entity/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entity
+{
+    public class Test:EntityBase
+    {
+        public string Name { get; set; }
+        public DateTime TestTime { get; set; }
+        public ICollection<Data> Datas { get; set; }
+        public Device Device { get; set; }
+    }
+}
